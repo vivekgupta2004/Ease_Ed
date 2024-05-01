3dee0ff0b20c5e50156981db6a5dfa86483002f5
mr_varshney2004 <130595295+vivekgupta2004@users.noreply.github.com>
2024-05-01 16:54:36 +0530

    create footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,15 @@ import Nav from "./components/Nav";
 import HeroSection from "./components/HeroSection";
 import About from "./components/About";
 import HowItWork from "./components/HowItWork";
-
+import { Footer } from "./components/Footer";
 function App() {
-
-
-
   return (
     <div className="bg-[#0D0C1A]  w-screen">
     <Nav/>
     <HeroSection/>
     <About/>
     <HowItWork/>
+    <Footer/>
     </div>
   )
 
