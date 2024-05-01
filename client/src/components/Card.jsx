e041d3393e4a501e228e186636d2be764b94f3ac
itz_Sambhav <sambhavsharma936@gmail.com>
2024-05-01 11:11:03 +0530

    responsive about

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className=' relative h-[70vh] w-1/3 rounded-lg border overflow-hidden'>
+    <div className=' relative h-[70vh] w-full md:w-1/3 rounded-lg border overflow-hidden'>
         <img src="/a-little-astronaut.png" alt="" className='h-full' />
         <h1 className='absolute bottom-28 left-8 text-3xl text-black'>Little Astro</h1>
-        <p className='absolute bottom-12 text-sm left-8 text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque consequuntur dolores expedita,Lorem ipsum dolor sit amet consectetur, </p>
+        <p className='absolute bottom-10 sm:bottom-8 md:bottom-12 text-sm left-8 text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque consequuntur dolores expedita,Lorem ipsum dolor sit amet consectetur, </p>
     </div>
   )
 }
