cf9a5cd167089dbe0304b15179f0790411899aad
itz_Sambhav <sambhavsharma936@gmail.com>
2024-05-02 20:21:47 +0530

    HardWorkIntialised

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -9,6 +9,8 @@ const Leaderboard = () => {
       <div className='h-[200vh] w-screen bg-[#0D0C1A] p-1'>
         <img src="/Ellipse14.png" alt="" className='absolute -top-[10vh] h-1/3 w-full left-0' />
         <Checkprogress/>
+
+        <div className='text-white mt-64'>HardWorkSection</div>
       </div>
     </div>
   )
