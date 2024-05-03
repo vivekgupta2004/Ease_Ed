bae4b6ee33bba3b07de530490287dc9eb4ed216c
itz_Sambhav <sambhavsharma936@gmail.com>
2024-05-03 14:02:08 +0530

    hardwork section done

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -5,64 +5,34 @@ import Checkprogress from './Checkprogress'
 const Leaderboard = () => {
   return (
     <div className='text-white'>
-      <Nav/>
+      <Nav />
       <div className='h-[300vh] w-screen bg-[#0D0C1A] p-1'>
         <img src="/Ellipse14.png" alt="" className='absolute -top-[10vh] h-1/3 w-full left-0' />
-        <Checkprogress/>
+        <Checkprogress />
 
-        <div className='text-white mt-64'>
-        <div>
-         <div className=' flex flex-row  items-center gap-40 ml-40  '>
-          <div className=' h-55 w-400 p-3 mr-10 '>
-            <h1 className='font- text-5xl mb-2 ' >Hard Work Section</h1>
-            <p className='font-bold text-base p-2 m-4'>Embrace the grind,for its's the path to <br /> greatness.</p>
-          </div>
-          <div className=' h-50 w-50'>
-            <img src="public/vvt.png" alt="" />
+        <div className='text-white mt-40 py-10 px-36'>
+          <div className='w-full flex items-center justify-between'>
+            <div className='w-full'>
+            <h1 className='text-5xl font-light'>Hard Work Section</h1>
+          <p className='w-1/4 mt-2 text-lg font-light ml-5 leading-6'>Embrace the grind, for it's the path to greatness.</p>
             </div>
-         </div>
-         <div className='  flex flex-row  items-center gap-40 ml-40 mb-9   '>
-         
-        <div className='border border-white-400 rounded-lg p-5 bg-green-500 '> 
-          <div className='flex flex-row  items-center justify-between gap-40' > <h1 className=' text-3xl mb-2 ' >Already Completed Task.</h1> 
-        <div>12:00-1:00</div>
+          <img src="/vvt.png" alt="" />
+          </div>
+
+          <div id="parentContainer" className='w-full h-fit  mt-10 flex items-center justify-center gap-10 flex-col p-5'>
+            <div className='w-full flex gap-64 items-center'>
+              <div className='card w-[60%] h-32 rounded-lg bg-[#15803D] flex justify-center py-7 gap-72'>
+                <div>
+                  <h1 className='text-3xl tracking-wide font-light'>Already Completed Class</h1>
+                  <p className='mt-1 font-semibold ml-1'>Completed</p>
+                </div>
+                <p className='font-semibold mt-2'>12:00-1:00</p>
+              </div>
+              <button className='h-1/2 border px-5 py-3 rounded-full'>Upload Work</button>
+            </div>
+          </div>
         </div>
-          <p className='font-bold text-base'>Completed</p></div>
-         
-         
-         <div className='border border-white-400 rounded-tl-lg rounded-tr-lg bg-green-500 '> 
-         <h3 className='text-base font-bold m-3 ' >Upload work</h3>
-         </div>
-         </div>
-         <div className='  flex flex-row  items-center gap-40 ml-40 mb-9   '>
-         
-        <div className='border border-white-400 rounded-lg p-5 bg-green-500 '> 
-          <div className='flex flex-row  items-center justify-between gap-40' > <h1 className=' text-3xl mb-2 ' >To  Be  Completed.    </h1> 
-        <div>12:00-1:00</div>
-        </div>
-          <p className='font-bold text-base'>Pending</p></div>
-         
-         
-         <div className='border border-white-400 rounded-tl-lg rounded-tr-lg bg-green-500 '> 
-         <h3 className='text-base font-bold m-3 ' >Upload work</h3>
-         </div>
-         </div>
-         <div className='  flex flex-row  items-center gap-40 ml-40 mb-9  '>
-         
-        <div className='border border-white-400 rounded-lg p-5 bg-green-500 '> 
-          <div className='flex flex-row  items-center justify-between gap-40' > <h1 className=' text-3xl mb-2 ' >Not Be Completed.</h1> 
-        <div>12:00-1:00</div>
-        </div>
-          <p className='font-bold text-base'>Incomplete</p></div>
-         
-         
-         <div className='border border-white-400 rounded-tl-lg rounded-tr-lg bg-green-500 '> 
-         <h3 className='text-base font-bold m-3 ' >Upload work</h3>
-         </div>
-         </div>
-        </div>
-        </div>
-       
+
 
       </div>
     </div>
