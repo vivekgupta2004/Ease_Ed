13e41a350e95d247c41fee59b68ef9c0b906d325
itz_Sambhav <sambhavsharma936@gmail.com>
2024-05-09 11:37:55 +0530

    upload work done

--- client/src/components/UploadWorkRe.jsx
+++ client/src/components/UploadWorkRe.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 function UploadWorkRe() {
     const title = useRef(null);
     const firstInput=useRef([0,1]);
+    console.log(firstInput);
     const handleSubmit = async (e) => {
         e.preventDefault();
         const response = await axios.post("http://localhost:3000/uploadtimetable", {
@@ -28,7 +29,7 @@ function UploadWorkRe() {
                 </div>
                 <form onSubmit={handleSubmit}>
                     <div className='h-20 w-full border-b-2 mb-4 flex items-center p-7'>
-                        <input className='hidden' ref={firstInput} type="text" />
+                        <input className='hidden' ref={firstInput} type="text" value={[0,1]}/>
                         <h1 className='text-2xl'>00:00-01:00</h1>
                         <input ref={title} className='ml-[10vw] border px-3 py-1 bg-transparent rounded-full text-lg' placeholder='Enter the title ' />
                         <button type='submit' className=' border px-5 py-3 rounded-full border-slate-500 flex items-center gap-4 font-semibold tracking-wide ml-44'>Upload <MdOutlineFileUpload className='scale-125' /></button>
